5ef5b71e8da0015c5812418e495cf42daa652b02
zhuzhen <939059477@qq.com>
2025-02-17 19:54:30 +0800

    feature:开始开发设备列表，还剩service层，controller没写完

--- src/main/java/com/ruoyi/project/device/controller/DeviceController.java
+++ src/main/java/com/ruoyi/project/device/controller/DeviceController.java
@@ -1,23 +1,15 @@
 package com.ruoyi.project.device.controller;
 
-import com.ruoyi.common.utils.poi.ExcelUtil;
-import com.ruoyi.common.utils.security.AuthorizationUtils;
 import com.ruoyi.framework.aspectj.lang.annotation.Log;
 import com.ruoyi.framework.aspectj.lang.enums.BusinessType;
 import com.ruoyi.framework.web.controller.BaseController;
 import com.ruoyi.framework.web.domain.AjaxResult;
-import com.ruoyi.framework.web.domain.Ztree;
 import com.ruoyi.framework.web.page.TableDataInfo;
-import com.ruoyi.project.system.dept.service.IDeptService;
-import com.ruoyi.project.system.role.domain.Role;
-import com.ruoyi.project.system.role.service.IRoleService;
-import com.ruoyi.project.system.user.domain.User;
-import com.ruoyi.project.system.user.domain.UserRole;
-import com.ruoyi.project.system.user.service.IUserService;
+import com.ruoyi.project.device.domain.Device;
+import com.ruoyi.project.device.service.IDeviceService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.annotation.Validated;
@@ -44,11 +36,7 @@ public class DeviceController extends BaseController
 {
     private String prefix = "device/list";
 
-    private final IRoleService roleService;
-
-    private final IUserService userService;
-
-    private final IDeptService deptService;
+    private final IDeviceService deviceService;
 
     /**
      * 界面
@@ -56,36 +44,15 @@ public class DeviceController extends BaseController
      */
     @RequiresPermissions("device:list:view")
     @GetMapping()
-    public String role()
+    public String device()
     {
         return prefix + "/device";
     }
 
-    @RequiresPermissions("system:role:list")
-    @PostMapping("/list")
-    @ResponseBody
-    public TableDataInfo list(Role role)
-    {
-        startPage();
-        List<Role> list = roleService.selectRoleList(role);
-        return getDataTable(list);
-    }
-
-    @Log(title = "角色管理", businessType = BusinessType.EXPORT)
-    @RequiresPermissions("system:role:export")
-    @PostMapping("/export")
-    @ResponseBody
-    public AjaxResult export(Role role)
-    {
-        List<Role> list = roleService.selectRoleList(role);
-        ExcelUtil<Role> util = new ExcelUtil<Role>(Role.class);
-        return util.exportExcel(list, "角色数据");
-    }
-
     /**
-     * 新增角色
+     * 新增设备
      */
-    @RequiresPermissions("system:role:add")
+    @RequiresPermissions("system:device:add")
     @GetMapping("/add")
     public String add()
     {
@@ -93,238 +60,110 @@ public class DeviceController extends BaseController
     }
 
     /**
-     * 新增保存角色
+     * 新增保存设备
      */
-    @RequiresPermissions("system:role:add")
-    @Log(title = "角色管理", businessType = BusinessType.INSERT)
+    @RequiresPermissions("system:device:add")
+    @Log(title = "设备管理", businessType = BusinessType.INSERT)
     @PostMapping("/add")
     @ResponseBody
-    public AjaxResult addSave(@Validated Role role)
+    public AjaxResult addSave(@Validated Device device)
     {
-        if (!roleService.checkRoleNameUnique(role))
+        if (!deviceService.checkNameUnique(device.getName()))
         {
-            return error("新增角色'" + role.getRoleName() + "'失败，角色名称已存在");
+            return error("新增设备'" + device.getName() + "'失败，设备名称已存在");
         }
-        else if (!roleService.checkRoleKeyUnique(role))
-        {
-            return error("新增角色'" + role.getRoleName() + "'失败，角色权限已存在");
-        }
-        AuthorizationUtils.clearAllCachedAuthorizationInfo();
-        return toAjax(roleService.insertRole(role));
+        return toAjax(deviceService.insert(device));
 
     }
 
-    /**
-     * 修改角色
-     */
-    @RequiresPermissions("system:role:edit")
-    @GetMapping("/edit/{roleId}")
-    public String edit(@PathVariable("roleId") Long roleId, ModelMap mmap)
-    {
-        roleService.checkRoleDataScope(roleId);
-        mmap.put("role", roleService.selectRoleById(roleId));
-        return prefix + "/edit";
-    }
-
-    /**
-     * 修改保存角色
-     */
-    @RequiresPermissions("system:role:edit")
-    @Log(title = "角色管理", businessType = BusinessType.UPDATE)
-    @PostMapping("/edit")
-    @ResponseBody
-    public AjaxResult editSave(@Validated Role role)
-    {
-        roleService.checkRoleAllowed(role);
-        roleService.checkRoleDataScope(role.getRoleId());
-        if (!roleService.checkRoleNameUnique(role))
-        {
-            return error("修改角色'" + role.getRoleName() + "'失败，角色名称已存在");
-        }
-        else if (!roleService.checkRoleKeyUnique(role))
-        {
-            return error("修改角色'" + role.getRoleName() + "'失败，角色权限已存在");
-        }
-        AuthorizationUtils.clearAllCachedAuthorizationInfo();
-        return toAjax(roleService.updateRole(role));
-    }
-
-    /**
-     * 角色分配数据权限
-     */
-    @RequiresPermissions("system:role:edit")
-    @GetMapping("/authDataScope/{roleId}")
-    public String authDataScope(@PathVariable("roleId") Long roleId, ModelMap mmap)
-    {
-        mmap.put("role", roleService.selectRoleById(roleId));
-        return prefix + "/dataScope";
-    }
-
-    /**
-     * 保存角色分配数据权限
-     */
-    @RequiresPermissions("system:role:edit")
-    @Log(title = "角色管理", businessType = BusinessType.UPDATE)
-    @PostMapping("/authDataScope")
-    @ResponseBody
-    public AjaxResult authDataScopeSave(Role role)
-    {
-        roleService.checkRoleAllowed(role);
-        roleService.checkRoleDataScope(role.getRoleId());
-        if (roleService.authDataScope(role) > 0)
-        {
-            setSysUser(userService.selectUserById(getUserId()));
-            return success();
-        }
-        return error();
-    }
-
-    @RequiresPermissions("system:role:remove")
-    @Log(title = "角色管理", businessType = BusinessType.DELETE)
+    @RequiresPermissions("system:device:remove")
+    @Log(title = "设备管理", businessType = BusinessType.DELETE)
     @PostMapping("/remove")
     @ResponseBody
     public AjaxResult remove(String ids)
     {
-        return toAjax(roleService.deleteRoleByIds(ids));
+        return toAjax(deviceService.deleteByIds(ids));
     }
 
     /**
-     * 校验角色名称
+     * 修改设备
      */
-    @PostMapping("/checkRoleNameUnique")
+    @RequiresPermissions("system:device:edit")
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable("id") Long id, ModelMap mmap)
+    {
+        //todo 数据权限
+        // deviceService.checkRoleDataScope(id);
+        mmap.put("device", deviceService.selectById(id));
+        return prefix + "/edit";
+    }
+
+    /**
+     * 修改保存设备
+     */
+    @RequiresPermissions("system:device:edit")
+    @Log(title = "设备管理", businessType = BusinessType.UPDATE)
+    @PostMapping("/edit")
     @ResponseBody
-    public boolean checkRoleNameUnique(Role role)
+    public AjaxResult editSave(@Validated Device device)
     {
-        return roleService.checkRoleNameUnique(role);
+        //todo 数据权限
+        // roleService.checkRoleAllowed(device);
+        // roleService.checkRoleDataScope(device.getRoleId());
+        if (!deviceService.checkNameUnique(device.getName()))
+        {
+            return error("修改设备'" + device.getName() + "'失败，设备名称已存在");
+        }
+        return toAjax(deviceService.update(device));
     }
 
     /**
-     * 校验角色权限
+     * 校验设备名称
      */
-    @PostMapping("/checkRoleKeyUnique")
+    @PostMapping("/checkNameUnique")
     @ResponseBody
-    public boolean checkRoleKeyUnique(Role role)
+    public boolean checkNameUnique(Device device)
     {
-        return roleService.checkRoleKeyUnique(role);
+        return deviceService.checkNameUnique(device.getName());
     }
 
-    /**
-     * 选择菜单树
-     */
-    @GetMapping("/selectMenuTree")
-    public String selectMenuTree()
-    {
-        return prefix + "/tree";
-    }
 
     /**
-     * 角色状态修改
+     * 设备状态修改
      */
-    @Log(title = "角色管理", businessType = BusinessType.UPDATE)
-    @RequiresPermissions("system:role:edit")
+    @Log(title = "设备管理", businessType = BusinessType.UPDATE)
+    @RequiresPermissions("system:device:edit")
     @PostMapping("/changeStatus")
     @ResponseBody
-    public AjaxResult changeStatus(Role role)
+    public AjaxResult changeStatus(Device device)
     {
-        roleService.checkRoleAllowed(role);
-        roleService.checkRoleDataScope(role.getRoleId());
-        return toAjax(roleService.changeStatus(role));
+        //todo 数据权限
+        // roleService.checkRoleAllowed(device);
+        // roleService.checkRoleDataScope(device.getRoleId());
+        //todo service未实现
+        // return toAjax(deviceService.changeStatus(device));
+        return null;
     }
 
-    /**
-     * 分配用户
-     */
-    @RequiresPermissions("system:role:edit")
-    @GetMapping("/authUser/{roleId}")
-    public String authUser(@PathVariable("roleId") Long roleId, ModelMap mmap)
-    {
-        mmap.put("role", roleService.selectRoleById(roleId));
-        return prefix + "/authUser";
-    }
 
-    /**
-     * 查询已分配用户角色列表
-     */
-    @RequiresPermissions("system:role:list")
-    @PostMapping("/authUser/allocatedList")
+    @RequiresPermissions("system:device:list")
+    @PostMapping("/list")
     @ResponseBody
-    public TableDataInfo allocatedList(User user)
+    public TableDataInfo list(Device device)
     {
         startPage();
-        List<User> list = userService.selectAllocatedList(user);
+        List<Device> list = deviceService.selectList(device);
         return getDataTable(list);
     }
 
-    /**
-     * 取消授权
-     */
-    @RequiresPermissions("system:role:edit")
-    @Log(title = "角色管理", businessType = BusinessType.GRANT)
-    @PostMapping("/authUser/cancel")
-    @ResponseBody
-    public AjaxResult cancelAuthUser(UserRole userRole)
-    {
-        return toAjax(roleService.deleteAuthUser(userRole));
-    }
-
-    /**
-     * 批量取消授权
-     */
-    @RequiresPermissions("system:role:edit")
-    @Log(title = "角色管理", businessType = BusinessType.GRANT)
-    @PostMapping("/authUser/cancelAll")
-    @ResponseBody
-    public AjaxResult cancelAuthUserAll(Long roleId, String userIds)
-    {
-        return toAjax(roleService.deleteAuthUsers(roleId, userIds));
-    }
-
-    /**
-     * 选择用户
-     */
-    @RequiresPermissions("system:role:list")
-    @GetMapping("/authUser/selectUser/{roleId}")
-    public String selectUser(@PathVariable("roleId") Long roleId, ModelMap mmap)
-    {
-        mmap.put("role", roleService.selectRoleById(roleId));
-        return prefix + "/selectUser";
-    }
-
-    /**
-     * 查询未分配用户角色列表
-     */
-    @RequiresPermissions("system:role:list")
-    @PostMapping("/authUser/unallocatedList")
-    @ResponseBody
-    public TableDataInfo unallocatedList(User user)
-    {
-        startPage();
-        List<User> list = userService.selectUnallocatedList(user);
-        return getDataTable(list);
-    }
-
-    /**
-     * 批量选择用户授权
-     */
-    @RequiresPermissions("system:role:edit")
-    @Log(title = "角色管理", businessType = BusinessType.GRANT)
-    @PostMapping("/authUser/selectAll")
-    @ResponseBody
-    public AjaxResult selectAuthUserAll(Long roleId, String userIds)
-    {
-        roleService.checkRoleDataScope(roleId);
-        return toAjax(roleService.insertAuthUsers(roleId, userIds));
-    }
-
-    /**
-     * 加载角色部门（数据权限）列表树
-     */
-    @RequiresPermissions("system:role:edit")
-    @GetMapping("/deptTreeData")
-    @ResponseBody
-    public List<Ztree> deptTreeData(Role role)
-    {
-        List<Ztree> ztrees = deptService.roleDeptTreeData(role);
-        return ztrees;
-    }
+    // @Log(title = "设备管理", businessType = BusinessType.EXPORT)
+    // @RequiresPermissions("system:device:export")
+    // @PostMapping("/export")
+    // @ResponseBody
+    // public AjaxResult export(Device device)
+    // {
+    //     List<Device> list = deviceService.selectList(device);
+    //     ExcelUtil<Device> util = new ExcelUtil<Device>(Device.class);
+    //     return util.exportExcel(list, "设备数据");
+    // }
 }
